ea4c7c2ae6b3522a583f83d200c49950d97f6147
luishi1 <luis.oviedo.et26@gmail.com>
2023-04-21 23:34:17 -0300

    agrego ejercicio 5 y 6

---
+++ app5/app5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app5
+{
+    internal class Program
+    {
+        static bool adivinada(string palabra, string letras)
+        {
+            foreach (char letra in palabra)
+            {
+                if (!letras.Contains(letra.ToString()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static string obtenerPalabra(string palabra, string letras)
+        {
+            string palabraAdivinada = "";
+
+            foreach (char letra in palabra)
+            {
+                if (letras.Contains(letra.ToString()))
+                {
+                    palabraAdivinada += letra;
+                }
+                else
+                {
+                    palabraAdivinada += "_ ";
+                }
+            }
+
+            return palabraAdivinada;
+        }
+        static void Main(string[] args)
+        {
+            //char admite operadores de comparación, igualdad, incremento y decremento. Además, en el caso de los operandos char, los operadores aritméticos y lógicos bit a bit realizan una operación en los códigos de caracteres correspondientes y producen el resultado del tipo int.
+            string[] palabras = { "manzana" , "amor" , "ordinario" , "dos" , "encantador" , "informacion" , "conocer" , "brillo" , "comediantes" , "renacer" };
+            string palabraOculta = palabras[new Random().Next(0, palabras.Length)];//genera cualquier de las opciones que posee el array de palabras
+            int intentos = 6; //cantidad de intentos posibles
+            string letras = "";
+            //mientras los intentos sean mayores a 0 y la funcion adivinada tire falso se cumple
+            while (intentos > 0 && !adivinada(palabraOculta, letras))
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese una letra para adivinar la palabra oculta");
+                //KeyChar propiedad para muestrear pulsaciones de tecla en tiempo de ejecución y para modificar las pulsaciones de tecla en circunstancias especiales en tiempo de ejecución.
+                //ingresamos la letra de la palbra
+                //readkey y que key char sirven para que solo ingree un digitoen caso que quiera entregar mas
+                char letraingresada = Console.ReadKey().KeyChar; 
+                Console.WriteLine();
+
+                //variaciones
+                if (palabraOculta.Contains(letraingresada))
+                {
+                    Console.WriteLine("Letra correcta");
+                    letras += letraingresada;
+                }
+                else
+                {
+                    Console.WriteLine("Letra incorrecta");
+                    intentos--;
+                }
+                Console.WriteLine("Te quedan "+ intentos +" intentos");
+                Console.WriteLine("Palabra adivinada: " + obtenerPalabra(palabraOculta, letras));
+                Console.ReadKey();
+
+
+                if (adivinada(palabraOculta, letras))
+                {
+                    Console.WriteLine("Felicidades acertaste la palabra " + palabraOculta);
+                }
+                else
+                {
+                    Console.WriteLine("Mal ahi no lograste adivinar la palabra , la palabra era "+ palabraOculta );
+                }
+            }
+        }
+    }
+}
---
+++ app6/app6/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app6
+{
+    internal class Program
+    {
+        class tareas
+        {
+            public string nombre;
+            public int prioridad;
+
+            public tareas(string nombre, int prioridad)
+            {
+             this.nombre = nombre;
+             this.prioridad = prioridad;
+            }
+
+            public void insertar(string nombre, int prio, List<tareas> list)
+            {
+                list.Add(new tareas(nombre, prio));
+            }
+
+          
+
+        }
+
+        static void Main(string[] args)
+        {
+            List<tareas>list = new List<tareas>();
+            tareas tarea1 = new tareas("ayuda",1);
+            list.Add(new tareas("dormir", 1));
+            list.Add(new tareas("comer", 1));
+            list.Add(new tareas("jugar a la play", 3));
+            list.Add(new tareas("deporte", 2));
+            list.Add(new tareas("estudiar", 1));
+            list.Add(new tareas("respirar", 3));
+            list.Add(new tareas("explotar la casa de gaspar", 1));
+
+            int opcion, opcion2;
+            string nombre;
+            int prioridad;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese que opcion quiere realizar");
+                Console.WriteLine("1. insertar nueva tarea");
+                Console.WriteLine("2. mostrar orden");
+                Console.WriteLine("3. salir");
+                opcion = int.Parse(Console.ReadLine());
+                if (opcion == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Inserter por favor el nombre de la tarea");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Inserter por favor la prioridad de la tarea");
+                    Console.WriteLine("SIGNIFICADOS");
+                    Console.WriteLine("1 = TAREA SUPER IMPORTANTE");
+                    Console.WriteLine("2 = TAREA MEDIANA IMPORTANTE");
+                    Console.WriteLine("3 = NO ES TAN IMPORTANCIA");
+                    Console.WriteLine("4 >= = EXISTE?");
+                    prioridad = int.Parse(Console.ReadLine());
+                    tarea1.insertar(nombre, prioridad,list);
+                    Console.ReadKey();
+                }
+                if (opcion == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Inserte por favor de la manera que desee que se ordene");
+                    Console.WriteLine("1 = por nombre de la tarea");
+                    Console.WriteLine("2 = por prioridad");
+                    opcion2 = int.Parse(Console.ReadLine());
+                    if (opcion2 == 1)
+                    {
+                        Console.Clear();
+                        list.Sort((x, y) => x.nombre.CompareTo(y.nombre));
+
+                        foreach (tareas t in list)
+                        {
+                            Console.WriteLine(t.nombre + " prioridad: " + t.prioridad);
+                        }
+                        Console.ReadKey();
+                    }
+                    if (opcion2 == 2)
+                    {
+                        Console.Clear();
+                        list.Sort((x, y) => x.prioridad.CompareTo(y.prioridad));
+
+                        foreach (tareas t in list)
+                        {
+                            Console.WriteLine(t.nombre + " prioridad: " + t.prioridad);
+                        }
+                        Console.ReadKey();
+                    }
+                }
+                if (opcion == 3)
+                {
+                    break;
+                }
+            }
+           
+        }
+
+    }
+}
+//6. Crear un programa que permita al usuario ingresar una lista de tareas y organizarlas por orden de prioridad.
