b2a730b6f649ebf32ff97b5be0323dbb9c22a720
luishi1 <luis.oviedo.et26@gmail.com>
2023-04-22 15:44:32 -0300

    subo ejercicio 9

--- app_9/app_9/Program.cs
+++ app_9/app_9/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace app_9
 {
@@ -10,6 +6,25 @@ namespace app_9
     {
         static void Main(string[] args)
         {
+            double x1;
+            double y1;
+            double x2;
+            double y2;
+            double r;
+            Console.WriteLine("Ingrese un valor para X1");
+            x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un valor para Y1");
+            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un valor para X2");
+            x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un valor para Y2");
+            y2 = double.Parse(Console.ReadLine());
+            r = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            //Math.Pow es un método en C# que calcula la potencia de un número
+            //Math.Sqrt es un método que calcula la raíz cuadrada de un número
+            Console.WriteLine(r + " Es la distancia que hay entre (" + x1 + ";" + y1+") y ("+x2 + ";" + y2 +")");
+
         }
     }
 }
+//crear un programa que permita al usuario calcular la distancia entre dos puntos en un plano cartesiano.
