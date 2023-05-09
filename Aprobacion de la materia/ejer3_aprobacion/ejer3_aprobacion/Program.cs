dde79c8f9aa25fee3f28f77ff4251edfa103e393
luishi1 <luis.oviedo.et26@gmail.com>
2023-05-08 23:33:09 -0300

    subo ejer 3 super ultra mego incompleto solo para tener la plantilla

---
+++ Aprobacion de la materia/ejer3_aprobacion/ejer3_aprobacion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer3_aprobacion
+{
+    internal class Program
+    {
+        class password
+        {
+            public int longitud { 
+            get { return longitud; }
+            set { longitud = value; }
+            }
+
+            public string contraseña
+            {
+                get { return contraseña; }
+                set { contraseña = value; }
+            }
+
+            public password() { }
+
+            public password (int longitud)
+            {
+                this.longitud = longitud;
+                Random rnd = new Random();
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
