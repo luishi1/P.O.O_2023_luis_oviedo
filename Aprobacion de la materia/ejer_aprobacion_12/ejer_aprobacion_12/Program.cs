6dfbb0e63533cb473083b0cb5ccba071d1078e05
luishi1 <luis.oviedo.et26@gmail.com>
2023-06-09 22:44:59 -0300

    subo ejercicios 12 y 13

---
+++ Aprobacion de la materia/ejer_aprobacion_12/ejer_aprobacion_12/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ejer_aprobacion_12
+{
+    public class Revolver
+    {
+        static Random random = new Random();
+        public int posicionActual;
+        public int posicionBala;
+
+        public Revolver()
+        {
+            posicionActual = GenerarActual();
+            posicionBala = GenerarPosBalas();
+        }
+
+        public int GenerarActual()
+        {
+            return random.Next(0, 6);
+        }
+
+        public int GenerarPosBalas()
+        {
+            return random.Next(0, 6);
+        }
+
+        public bool Disparar()
+        {
+            return posicionActual == posicionBala;
+        }
+        public void siguienteBala()
+        {
+            posicionActual = (posicionActual + 1) % 6;
+        }
+    }
+
+    public class Jugadores
+    {
+        static Random rand = new Random();
+        public int ID;
+        public string nombre;
+        public bool tavivo;
+
+        public Jugadores(int ID)
+        {
+            this.ID = ID;
+            nombre = GenerarNombre();
+            tavivo = true;
+        }
+
+        public string GenerarNombre()
+        {
+            string[] nombres =
+            { "Felipe" , "Tobias" , "Agustin" , "Lucio" , "Marcos" , "Leandro" , "Gaspar" , "Thiago" , "Evelyn" , "Sebastian" , "Joaquin" , "Santiago" ,
+            "Matias" , "Facundo" , "Claudia" , "Azul" , "Sofia" , "Tamara" , "Jorge" , "Gabriel" , "Angie" , "Aaron" , "Luis" , "Pablo" , "Esteban" , "Emma" , "Graciela" , "Estella" , "Sandra" };
+            string nombre = nombres[rand.Next(nombres.Length)] + " " + ID;
+            return nombre;
+        }
+
+        public void Disparar(Revolver revolver, ref bool juegoTerminado)
+        {
+            if (tavivo)
+            {
+                if (revolver.Disparar())
+                {
+                    tavivo = false;
+                    juegoTerminado = true;
+                    Console.WriteLine("El jugador " + nombre + " ha MUERTO");
+                }
+                else
+                {
+                    revolver.siguienteBala();
+                    Console.WriteLine("El jugador " + nombre + " sigue VIVO");
+                }
+            }
+        }
+
+    }
+
+    public class Juego
+    {
+        public List<Jugadores> jugadores;
+        public Revolver Revolver;
+
+        public Juego()
+        {
+            jugadores = new List<Jugadores>();
+            Revolver = new Revolver();
+        }
+
+        public bool FinJuego()
+        {
+            foreach (Jugadores j in jugadores)
+            {
+                if (j.tavivo == false)
+                {
+                    Console.WriteLine("El juego termino el jugador " + j.nombre + " a MUERTO");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ronda()
+        {
+            bool juegoTerminado = false;
+            int contador = 1;
+            while (!juegoTerminado)
+            {
+                foreach (Jugadores J in jugadores)
+                {
+                    Console.WriteLine("Ronda " + contador);
+                    Console.WriteLine("Nombre del Jugador " + J.nombre);
+                    Console.Write("Disparo.......");
+                    Console.ReadLine();
+                    J.Disparar(Revolver, ref juegoTerminado);
+                    if (!J.tavivo)
+                    {
+                        Console.WriteLine("Estado del Jugador: Muerto");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Estado del Jugador: Vivo");
+                        Console.WriteLine("");
+                    }
+                    if (juegoTerminado)
+                        break;
+                }
+                contador++;
+            }
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Juego juego = new Juego();
+            List<Jugadores> personas = juego.jugadores;
+            Console.WriteLine("Ingrese la cantidad de jugadores que van a participar en el juego");
+            int cantidad = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cantidad; i++)
+            {
+                Jugadores jugador = new Jugadores(i + 1);
+                personas.Add(jugador);
+            }
+            while (true)
+            {
+                Console.Clear();
+                juego.ronda();
+                Console.WriteLine();
+                if (juego.FinJuego())
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+/*
+Use una variable bandera para que en el metodo disparar de jugador
+me indique si el juego acaba o no asi si un jugador muere no lo continue
+
+el termine ref se usa para pasar una referencia de una variable
+dentro del metodo si recibe una modificacion se modifica el valor de la misma
+ */
---
+++ Aprobacion de la materia/ejer_aprobacion_13/ejer_aprobacion_13/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejer_aprobacion_13
+{
+    public class Empleado
+    {
+        private static readonly Random random = new Random();
+        public const double PLUS = 300;
+
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+
+        public Empleado() { }
+
+        public Empleado(string nombre, int edad, double salario)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Salario = salario;
+        }
+
+        public Empleado(double salario)
+        {
+            Nombre = GenerarNombre();
+            Edad = GenerarEdad();
+            Salario = salario;
+        }
+
+        public virtual void SumarPlus()
+        {
+            Salario += PLUS;
+        }
+
+        private static string GenerarNombre()
+        {
+            string[] nombres =
+            {
+                "Felipe", "Tobias", "Agustin", "Lucio", "Marcos", "Leandro", "Gaspar", "Thiago", "Evelyn", "Sebastian",
+                "Joaquin", "Santiago", "Matias", "Facundo", "Claudia", "Azul", "Sofia", "Tamara", "Jorge", "Gabriel",
+                "Angie", "Aaron", "Luis", "Pablo", "Esteban", "Emma", "Graciela", "Estella", "Sandra", "Goku"
+            };
+            return nombres[random.Next(nombres.Length)];
+        }
+
+        private static int GenerarEdad()
+        {
+            return random.Next(18, 70);
+        }
+
+        public virtual void Mostrar()
+        {
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Edad: " + Edad);
+            Console.WriteLine("Salario: " + Salario);
+        }
+    }
+
+    public class Comercial : Empleado
+    {
+        public double Comision { get; set; }
+
+        public Comercial() : base() { }
+
+        public Comercial(string nombre, int edad, double salario, double comision) : base(nombre, edad, salario)
+        {
+            Comision = comision;
+        }
+
+        public Comercial(double salario, double comision) : base(salario)
+        {
+            Comision = comision;
+        }
+
+        public override void SumarPlus()
+        {
+            if (Edad > 30 && Comision > 200)
+            {
+                Salario += PLUS;
+            }
+        }
+
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("Comision: " + Comision);
+        }
+    }
+
+    public class Repartidor : Empleado
+    {
+        public string Zona { get; set; }
+
+        public Repartidor() : base() { }
+
+        public Repartidor(string nombre, int edad, double salario, string zona) : base(nombre, edad, salario)
+        {
+            Zona = zona;
+        }
+
+        public Repartidor(double salario, string zona) : base(salario)
+        {
+            Zona = zona;
+        }
+
+        public override void SumarPlus()
+        {
+            if (Edad > 25 && Zona == "zona 3")
+            {
+                Salario += PLUS;
+            }
+        }
+
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("Zona: " + Zona);
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Comercial> comerciantes = new List<Comercial>();
+            List<Repartidor> repartidores = new List<Repartidor>();
+
+            Comercial comercial1 = new Comercial(233, 339);
+            Comercial comercial2 = new Comercial("Cell", 45, 500, 435);
+            Comercial comercial3 = new Comercial(520, 140);
+            Repartidor repartidor1 = new Repartidor(233, "zona 3");
+            Repartidor repartidor2 = new Repartidor("Ballin", 20, 500, "zona 3");
+            Repartidor repartidor3 = new Repartidor(520, "zona 3");
+
+            comerciantes.Add(comercial1);
+            comerciantes.Add(comercial2);
+            comerciantes.Add(comercial3);
+
+            repartidores.Add(repartidor1);
+            repartidores.Add(repartidor2);
+            repartidores.Add(repartidor3);
+
+            Console.WriteLine("Repartidores");
+            Console.WriteLine();
+
+            foreach (Repartidor repartidor in repartidores)
+            {
+                Console.WriteLine("---------------");
+                repartidor.Mostrar();
+                repartidor.SumarPlus();
+                Console.WriteLine();
+                Console.WriteLine("Salario POST-PLUS");
+                Console.WriteLine();
+                repartidor.Mostrar();
+            }
+            Console.WriteLine("---------------");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Comerciantes");
+            Console.WriteLine();
+
+            foreach (Comercial comercial in comerciantes)
+            {
+                Console.WriteLine("---------------");
+                comercial.Mostrar();
+                comercial.SumarPlus();
+                Console.WriteLine();
+                Console.WriteLine("Salario POST-PLUS");
+                Console.WriteLine();
+                comercial.Mostrar();
+            }
+
+            Console.WriteLine("---------------");
+        }
+    }
+}
